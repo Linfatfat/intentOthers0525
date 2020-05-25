8372656d687b0d6e35a3d763a9ec99842c5a0ab7
Linfatfat <s940110118@gmail.com>
2020-05-25 11:22:59 +0800

    第一次完成

---
+++ app/src/main/java/com/u1065115/intentothers0525/MainActivity.java
@@ -0,0 +1,44 @@
+package com.u1065115.intentothers0525;
+
+import android.app.DownloadManager;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnOK(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        switch (view.getId()){
+            case R.id.btnEmail:
+                intent.setData(Uri.parse("mailto:s940110118@gmail.com"));
+                intent.putExtra(Intent.EXTRA_SUBJECT,"'哈囉");
+                intent.putExtra(Intent.EXTRA_TEXT,"'你好嗎?");
+                break;
+
+             case  R.id.btnWeb:
+                 intent.setData(Uri.parse("http://www.csim.hk.edu.tw/main.php"));
+                 break;
+
+             case  R.id.btnMap:
+                 intent.setData(Uri.parse("https://goo.gl/maps/gZA7BchBu3GxF4Kh7"));
+
+            case  R.id.btnWebSearch:
+                intent.setAction(Intent.ACTION_WEB_SEARCH);
+                intent.putExtra(SearchManager.QUERY,"弘光科技大學");
+
+                break;
+
+        }
+        startActivity(intent);
+    }
+}
